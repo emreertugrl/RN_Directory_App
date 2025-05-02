e344834a7a3574e8af186c02dcd7159f1c08aa0b
emreertugrl <emrertugrl@gmail.com>
2025-05-02 14:36:06 +0300

    default style and colors defined

--- src/styles/defaultScreenStyle.js
+++ src/styles/defaultScreenStyle.js
@@ -4,7 +4,6 @@ import Colors from '../theme/colors';
 const defaultScreenStyle = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.WHITE,
     padding: 10,
   },
 });
--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,4 +1,8 @@
 const Colors = {
   WHITE: '#FFFFFF',
+  BLUE: '#1B56FD',
+  GRAY: '#AAA',
+  RED: '#DD0000',
+  GREEN: '#37d67a',
 };
 export default Colors;
